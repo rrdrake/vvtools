57bc638bac4fe1c5b5111eb0611dd0f6e9de26e7
Richard R. Drake <rrdrake@sandia.gov>
2018-11-25 06:21:34 -0700

    workaround issue PYTHONPATH and colons
    The path separator for PYTHONPATH is a colon, which creates a problem
    if you want to add paths that contain colons.  For vvtest, if the
    directory you are running the tests contains a colon, the tests will
    not be able to import vvtest_util.py.
    
    This commit adds a workaround for this case, by
    
     1. make sure your test imports script_util before vvtest_util
     2. script_util/__init__.py adjusts the sys.path
    
    The adjustment uses the original PYTHONPATH set by vvtest and the fact
    that vvtest only adds absolute paths to PYTHONPATH.

--- vvt/config/script_util/__init__.py
+++ vvt/config/script_util/__init__.py
@@ -4,5 +4,29 @@
 # (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
 # Government retains certain rights in this software.
 
+
+# this section adjusts sys.path to work around python's algorithm for
+# processing PYTHONPATH, which does weird things if paths contain colons.
+# the logic here assumes all paths in PYTHONPATH are absolute
+
+import os as _os
+import sys as _sys
+
+dL = _os.environ.get( 'PYTHONPATH', '' ).split( ':/' )
+
+cnt = 0
+for i,d in enumerate(dL):
+    if i > 0:
+        dL[i] = '/'+dL[i]
+    cnt += d.count( ':' )
+
+if cnt > 0:
+    dL.reverse()
+    for d in dL:
+        _sys.path.insert( 1, d )
+
+
+# for convenience, add symbols from each submodule
+
 from .standard_utilities import *
 from .simple_aprepro import simple_aprepro
